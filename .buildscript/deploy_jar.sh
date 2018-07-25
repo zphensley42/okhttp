21b6ce238abc6f8041e016e136fbd07c0826dfb6
Zachary Hensley <zachary.hensley@siriusxm.com>
2018-07-25 10:05:58 -0500

    Added script to deploy lfs files for builds

---
+++ .buildscript/deploy_jar.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+pushd ./ || exit
+
+cp ../benchmarks/target/*.jar ../deploy_jar/
+cp ../mockwebserver/target/*.jar ../deploy_jar/
+cp ../okcurl/target/*.jar ../deploy_jar/
+cp ../okhttp/target/*.jar ./../deploy_jar/
+cp ../okhttp-android-support/target/*.jar ./../deploy_jar/
+cp ../okhttp-apache/target/*.jar ./../deploy_jar/
+cp ../okhttp-dnsoverhttps/target/*.jar ./../deploy_jar/
+cp ../okhttp-hpacktests/target/*.jar ./../deploy_jar/
+cp ../okhttp-logging-interceptor/target/*.jar ./../deploy_jar/
+cp ../okhttp-sse/target/*.jar ./../deploy_jar/
+cp ../okhttp-testing-support/target/*.jar ./../deploy_jar/
+cp ../okhttp-tests/target/*.jar ./../deploy_jar/
+cp ../okhttp-tls/target/*.jar ./../deploy_jar/
+cp ../okhttp-urlconnection/target/*.jar ./../deploy_jar/
+
+popd || exit
